5ac3289a40b034b5170026760a472d1d8ac6c32b
asrithapenugonda <asrithapenugonda16121@gmail.com>
2026-09-19 08:15:21 +0100

    variable.sh commit

--- variable.sh
+++ variable.sh
@@ -2,3 +2,14 @@ name="Asritha Jaykumar"
 age=27
 
 echo " My name is $name and I'm $age"
+
+name = Asritha #no spaces must there when assigning variables
+
+date_time=$date
+echo " Current date and time are $date_time"
+
+current_date=$(date +%Y-%m-%d)
+current_time=$(date +%H:%M:%S)
+
+echo -e "\e[31m Current date is $current_date\e[0m"
+echo -e "\e[32m Current time is $current_time\e[0m"
