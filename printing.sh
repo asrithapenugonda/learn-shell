98ef17b4963b49094321a41acfeaee8c998e4295
asrithapenugonda <asrithapenugonda16121@gmail.com>
2026-09-19 07:52:15 +0100

    printing.sh commit1

--- printing.sh
+++ printing.sh
@@ -1,6 +1,27 @@
 echo " hello    world"
 echo hello          world
 echo 10
-echo -e "\nhellloworld"
+echo -e "helllo\nworld"
 echo -e "\thello world"
+
+echo -e "Today's date is $date"
+
+echo -e "\e[31mHello world\e[0m"
+
+echo -e "\e[32mHelllo world\e[0m"
+
+echo -e "\e[33mHello world\e[0m"
+
+echo -e "\e[34mHello world\e[0m"
+
+echo -e "\e[35mHello world\e[0m"
+echo -e "\e[36mHello world\e[0m"
+echo -e "\e[37mHello world\e[0m"
+echo -e "\e[38mHello world\e[0m"
+echo -e "\e[39mHello world\e[0m"
+echo -e "\e[40mHello world\e[0m"
+echo -e "\e[41mHello world\e[0m"
+echo -e "\e[42mHello world\e[0m"
+
+
 
